321652f16319dca5323710719135eec4a7818859
Adrian <ar.hordyk@gmail.com>
2019-05-06 15:20:24 -0700

    Update control.R

--- R/control.R
+++ R/control.R
@@ -1,8 +1,19 @@
 
+loadPackages <- function(pcks=NUL) {
+  for (pk in pcks) {
+    Chk <- require(pk, character.only=TRUE)
+  }
+  if (!Chk) {
+    install.packages(pk)
+    require(pk, character.only=TRUE)
+  }
+}
+
+pcks <- c('DLMtool', 'MSEtool', 'dplyr', 'ggplot2', 
+          'flextable', 'forcats', 'grid', 'gridExtra')
+
 # Load libraries
-library(DLMtool); library(dplyr); library(ggplot2)
-library(MSEtool); library(flextable); library(forcats);
-library(grid); library(gridExtra)
+loadPackages(pcks)
 
 # Directory names
 OMdir <- "OMs"
@@ -11,7 +22,6 @@ Resultsdir <- "Results"
 nsim <- 100
 
 source("R/Functions.R")
-
 
 # Create MP 
 SCA_MP <- make_MP(SCA, HCR_MSY, fix_h=TRUE, fix_tau=TRUE)
